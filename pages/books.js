d91d02593eb9aa7ee77db1b7524e7944ffb3627d
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-08 16:46:32 +0000

    replaced <a> tag with <Link>

--- pages/books.js
+++ pages/books.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function BooksPage() {
   return (
     <div>
       <div className="bg-green-50">Books Page</div>
       <div>
-        <a href="/">Go back to Home Page</a>
+        <Link href="/">Go back to Home Page</Link>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <div className="bg-red-100 text-2xl">Hello👋</div>
       <div>
-        <a href="/books">Go to books page</a>
+        <Link href="/books">Go to books page</Link>
       </div>
     </div>
   );
