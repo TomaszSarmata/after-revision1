6131d6e0f2a1f0aad9858dd2088661aff2f2eb0f
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-08 16:59:08 +0000

    added button to programaticaly navigate the user to books page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    alert("You clicked the button! I will now navigate you to the right page");
+    router.push("/books");
+  };
+
   return (
     <div>
+      <button className="bg-blue-500 px-3 py-1 rounded" onClick={handleClick}>
+        Click Me!
+      </button>
       <div className="bg-red-100 text-2xl">Hello👋</div>
       <div>
         <Link href="/books">Go to books page</Link>
