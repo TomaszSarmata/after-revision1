cdbdf001b2e8755e53dda9b367cdcdb7176b38ce
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-09 10:05:36 +0000

    moved books arr to the api endpoint

---
+++ components/books/books-grid/books.js
@@ -0,0 +1,16 @@
+const books = [
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+  },
+  {
+    title: "The Fellowship of the Ring",
+    author: "J.R.R. Tolkien",
+  },
+  {
+    title: "Born a Crime",
+    author: "Trevor Noah",
+  },
+];
+
+export default books;
