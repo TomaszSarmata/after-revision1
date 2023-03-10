5fce7b04a2f14e741594ab6c33e755fbd7a51aae
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-10 14:07:01 +0000

    added image to each book-item to  display dynamicaly

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -1,8 +1,9 @@
 export default function BookItem({ book }) {
   return (
-    <div className="w-full bg-red-100">
-      <p>{book.title}</p>
-      <p>{book.author}</p>
+    <div className="w-full border rounded-lg p-4">
+      <img src={book.imgUrl} className="rounded-lg" />
+      <p className="text-center text-xl font-medium ">{book.title}</p>
+      <p className="text-center text-gray-700">{book.author}</p>
     </div>
   );
 }
