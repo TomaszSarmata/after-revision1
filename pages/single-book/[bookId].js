bc54aae970134cd1a894b1e21cf4d7f5c2080de0
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-11 13:24:43 +0000

    added copy to clipboard react library and created a button to allow a user to copy a link to amazon

--- pages/single-book/[bookId].js
+++ pages/single-book/[bookId].js
@@ -4,6 +4,7 @@ import Footer from "@/components/shared/footer";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Content from "@/components/shared/content";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function SingleBook() {
   const router = useRouter();
@@ -33,6 +34,8 @@ export default function SingleBook() {
 
   //TODO make another sanity check to make sure we get an error for the user coming from the endpoint if the singleBook.id goes beyond the array.length (problem: how to refer to the array if its not imported here)
 
+  console.log("here", singleBook);
+
   return (
     <div>
       <Header title={singleBook.title}></Header>
@@ -41,10 +44,21 @@ export default function SingleBook() {
         title={singleBook.title}
         subtitle={singleBook.author}
       ></Hero>
+
       <Content>
         <div className="w-full flex flex-col ">
-          <div className="w-2/12 bg-blue-500 rounded-lg text-center">
-            <a href="https://amzn.eu/d/9lMfPCF" target="_blank">
+          <div className="w-full">
+            <CopyToClipboard text={singleBook.linkToPurchase}>
+              <button
+                className="bg-blue-500 text-white rounded-lg text-center w-2/12 mb-5 py-1"
+                type="button"
+              >
+                Copy link to Amazon
+              </button>
+            </CopyToClipboard>
+          </div>
+          <div className="bg-blue-500 rounded-lg text-center text-white w-2/12 py-1">
+            <a href={singleBook.linkToPurchase} target="_blank">
               Buy on Amazon
             </a>
           </div>
