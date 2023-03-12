9420afd5c427d04a5013ff9812af1e888d1944bc
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-12 09:43:43 +0000

    moved the books list over to our db

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,28 +1,32 @@
 import sql from "@/utils/postrgres";
-export default function handler(req, res) {
-  const initialBooks = [
-    {
-      id: "1",
-      title: "The Hobbit",
-      author: "J.R.R. Tolkien",
-      imgUrl: "/assets/1.png",
-      linkToPurchase: "https://amzn.eu/d/9lMfPCF",
-    },
-    {
-      id: "2",
-      title: "The Fellowship of the Ring",
-      author: "J.R.R. Tolkien",
-      imgUrl: "/assets/2.png",
-      linkToPurchase: "https://amzn.eu/d/9lMfPCF",
-    },
-    {
-      id: "3",
-      title: "Born a Crime",
-      author: "Trevor Noah",
-      imgUrl: "/assets/3.png",
-      linkToPurchase: "https://amzn.eu/d/9lMfPCF",
-    },
-  ];
+export default async function handler(req, res) {
+  const initialBooks = await sql`
+      SELECT * FROM books
+  `;
+
+  // const initialBooks = [
+  //   {
+  //     id: "1",
+  //     title: "The Hobbit",
+  //     author: "J.R.R. Tolkien",
+  //     imgUrl: "/assets/1.png",
+  //     linkToPurchase: "https://amzn.eu/d/9lMfPCF",
+  //   },
+  //   {
+  //     id: "2",
+  //     title: "The Fellowship of the Ring",
+  //     author: "J.R.R. Tolkien",
+  //     imgUrl: "/assets/2.png",
+  //     linkToPurchase: "https://amzn.eu/d/9lMfPCF",
+  //   },
+  //   {
+  //     id: "3",
+  //     title: "Born a Crime",
+  //     author: "Trevor Noah",
+  //     imgUrl: "/assets/3.png",
+  //     linkToPurchase: "https://amzn.eu/d/9lMfPCF",
+  //   },
+  // ];
 
   res.json(initialBooks);
 }
