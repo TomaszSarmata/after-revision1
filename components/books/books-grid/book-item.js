98542df0b2a76880e989027574aeb4d2f28c7528
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-12 14:30:42 +0000

    added Link tag to each book-item to allow navigation to the single-book page

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Link from "next/link";
 
 export default function BookItem({ book }) {
   const [like, setLike] = useState(0);
@@ -10,7 +11,9 @@ export default function BookItem({ book }) {
 
   return (
     <div className="w-full border rounded-lg p-4 flex flex-col justify-center items-center gap-y-1">
-      <img src={book.img_url} className="rounded-lg" />
+      <Link href={`/single-book/${book.id}`}>
+        <img src={book.img_url} className="rounded-lg" />
+      </Link>
       <p className="text-center text-xl font-medium ">{book.title}</p>
       <p className="text-center text-gray-700">{book.author}</p>
       <button
