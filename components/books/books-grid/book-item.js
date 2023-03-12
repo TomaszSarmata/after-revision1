aa556cc2dc8cc12ab193915537e96e25542ee632
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-12 09:43:59 +0000

    fixed a bug

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -10,7 +10,7 @@ export default function BookItem({ book }) {
 
   return (
     <div className="w-full border rounded-lg p-4 flex flex-col justify-center items-center gap-y-1">
-      <img src={book.imgUrl} className="rounded-lg" />
+      <img src={book.img_url} className="rounded-lg" />
       <p className="text-center text-xl font-medium ">{book.title}</p>
       <p className="text-center text-gray-700">{book.author}</p>
       <button
