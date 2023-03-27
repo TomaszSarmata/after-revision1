9d54e3b8736c153cb407ae00a4544a446562b93e
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-27 19:36:32 +0100

    wired up input with react state in search page

--- pages/search.js
+++ pages/search.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 
 export default function Search() {
   const [locations, setLocations] = useState([]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     getLocations();
@@ -28,7 +29,7 @@ export default function Search() {
       <Header title="Search"></Header>
       <Content>
         <div className="w-full sm:w-8/12 md:w-6/12 lg:w-4/12 flex flex-row space-x-2">
-          <Input></Input>
+          <Input value={input} onChange={(value) => setInput(value)}></Input>
           <button
             className="bg-blue-500 text-white rounded px-6 py-2"
             type="button"
@@ -38,7 +39,10 @@ export default function Search() {
         </div>
         <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
           {locations.map((location) => (
-            <div key={location.id} className="w-full h-40 bg-red-100"></div>
+            <div key={location.id} className="w-full h-40 bg-red-100">
+              <img src={location.img_url} alt="" />
+              <p>{location.title}</p>
+            </div>
           ))}
         </div>
       </Content>
